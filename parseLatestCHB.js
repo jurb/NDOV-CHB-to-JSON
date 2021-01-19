a5609314e5a8691411ca38905711884f15a0c14d
Jurian Baas <jurian.baas@gmail.com>
2021-01-19 12:05:04 +0100

    added more transform operations: filter, coordinates, smaller extra file output

--- parseLatestCHB.js
+++ parseLatestCHB.js
@@ -7,6 +7,9 @@ const gunzip = require("gunzip-file");
 // [xml2js](https://www.npmjs.com/package/xml2js) transforms XML to JSON
 const xml2js = require("xml2js");
 
+// import script to convert from rd to wsg84 coordinates
+const rdToWgs84 = require("rd-to-wgs84");
+
 // This script aims to
 // * download all files from https://data.ndovloket.nl/haltes/ periodically
 // * Of those files, rename the latest public transport information file (ExportCHB *.xml.gz) to a 'Latest' version
@@ -18,6 +21,7 @@ const xml2js = require("xml2js");
 // See https://github.com/BISONNL/CHB for the xsd of the xml.
 
 // We'd like to download the contents of the given URL, and keep only the lates version of the Export*.xml.gz file.
+
 const scraperOptions = {
   urls: ["https://data.ndovloket.nl/haltes/"],
   urlFilter: function (url) {
@@ -31,12 +35,12 @@ const scraperOptions = {
 
 // file name constants
 const scrapeDir = "scrapeNDOV";
-const ExportCHBLatestName = `ExportCHBLatest`;
-const ExportCHBLatestZip = `${ExportCHBLatestName}.xml.gz`;
-const ExportCHBLatestXML = `${ExportCHBLatestName}.xml`;
+const constantLatestString = `ExportCHBLatest`;
+const constantLatestStringZip = `${constantLatestString}.xml.gz`;
+const constantLatestStringXML = `${constantLatestString}.xml`;
 
 // function to get the newest (simple sorted) ExportCHB* filename
-const getLatestCHBExport = () =>
+const latestCHBExportName = () =>
   fs
     .readdirSync(scrapeDir)
     .filter((el) => el.slice(0, 9) === "ExportCHB")
@@ -46,11 +50,11 @@ const getLatestCHBExport = () =>
 // function to copy CHB file to root directory
 const copyLatest = () =>
   fs.copyFile(
-    `${scrapeDir}/${getLatestCHBExport()}`,
-    ExportCHBLatestZip,
+    `${scrapeDir}/${latestCHBExportName()}`,
+    constantLatestStringZip,
     (el) =>
       console.log(
-        `${scrapeDir}/${getLatestCHBExport()} downloaded and copied to /${ExportCHBLatestZip}`
+        `${scrapeDir}/${latestCHBExportName()} downloaded and copied to /${constantLatestStringZip}`
       )
   );
 
@@ -69,15 +73,31 @@ const parser = new xml2js.Parser({
 });
 
 const parseCHBXMLtoJSON = () =>
-  fs.readFile(`${__dirname}/${ExportCHBLatestXML}`, function (err, data) {
+  fs.readFile(`${__dirname}/${constantLatestStringXML}`, function (err, data) {
     parser.parseString(data, function (err, result) {
+      // just filter on Amsterdam so we have a json with all properties
+      const filteredResultsAllProperties = result.export.stopplaces.stopplace.filter(
+        (stopplace) => stopplace.stopplacename.town === "Amsterdam"
+      );
+
+      ///////////////////////////////
+      // write away current and latest version, for versioning
       fs.writeFile(
-        `${ExportCHBLatestName}-amsterdam.json`,
+        `${constantLatestString}-amsterdam.json`,
         JSON.stringify(
           // Only include results from Amsterdam
-          result.export.stopplaces.stopplace.filter(
-            (el) => el.stopplacename.town === "Amsterdam"
-          )
+          filteredResultsAllProperties
+        ),
+        "utf8",
+        function (err) {
+          if (err) console.error(err);
+        }
+      );
+      fs.writeFile(
+        `${latestCHBExportName()}-amsterdam.json`,
+        JSON.stringify(
+          // Only include results from Amsterdam
+          filteredResultsAllProperties
         ),
         "utf8",
         function (err) {
@@ -85,7 +105,109 @@ const parseCHBXMLtoJSON = () =>
         }
       );
 
-      // console.dir(autotypeResult);
+      ///////////////////////////////
+      // Also create a version with flattened quays, still filtered on Amsterdam
+      const quaysSelectionAmsterdam = filteredResultsAllProperties
+        .map((stopplace) => {
+          const { quays, ...stop } = stopplace; // destructure quays and stop objects in own constants
+          const quaysWrapped =
+            { ...quays }["quay"] instanceof Array
+              ? { ...quays }["quay"]
+              : [{ ...quays }["quay"]]; // if there is a single quay object, put it in an array so we can map over it
+
+          const quaysWithStop = quaysWrapped
+            .filter((el) => el) // remove empty arrays
+            .filter((el) => el.quaystatusdata.quaystatus !== "outofuse") // filter out quays that are out of use
+            .map((quay) => ({
+              // map over each quay
+              ...quay, // destructure whole quay object
+              stopplace: stop, // also add the stopplace information
+              geo: rdToWgs84(
+                quay.quaylocationdata["rd-x"],
+                quay.quaylocationdata["rd-y"]
+              ), // add wsg84 coordinates
+              direction: getCardinalDirectionShort(
+                quay.quaybearing.compassdirection
+              ),
+              directionfull: getCardinalDirectionFull(
+                quay.quaybearing.compassdirection
+              ),
+            }));
+          return quaysWithStop;
+        })
+        .flat(); // flaten nested quay arrays
+
+      // write away current and latest version, for versioning
+      fs.writeFile(
+        `${constantLatestString}-quays-selection-amsterdam.json`,
+        JSON.stringify(
+          // Only include results from Amsterdam
+          quaysSelectionAmsterdam
+        ),
+        "utf8",
+        function (err) {
+          if (err) console.error(err);
+        }
+      );
+      fs.writeFile(
+        `${latestCHBExportName()}-quaysselection-amsterdam.json`,
+        JSON.stringify(
+          // Only include results from Amsterdam
+          quaysSelectionAmsterdam
+        ),
+        "utf8",
+        function (err) {
+          if (err) console.error(err);
+        }
+      );
+
+      ///////////////////////////////
+      // Also create a version with flattened quays and selected properties, still filtered on Amsterdam
+      const quaysAndPropsSelectionAmsterdam = quaysSelectionAmsterdam.map(
+        (quay) => ({
+          // map over each quay
+          // ...quay, // destructure whole quay object
+          quaycode: quay.quaycode,
+          quayname: quay.quaynamedata.quayname, // also add the stopplace information,
+          quaystatus: quay.quaystatusdata.quaystatus,
+          transportmode:
+            quay.quaytransportmodes.transportmodedata.transportmode,
+          lat: quay.geo.lat,
+          lon: quay.geo.lon,
+          visuallyaccessible:
+            quay.quayvisuallyaccessible.visuallyaccessible === "Y",
+          disabledaccessible:
+            quay.quaydisabledaccessible.disabledaccessible === "Y",
+          compassdirection: quay.quaybearing.compassdirection,
+          ...quay.quayaccessibilityadaptions,
+          direction: quay.direction,
+          directionfull: quay.directionfull,
+        })
+      );
+
+      // write away current and latest version, for versioning
+      fs.writeFile(
+        `${constantLatestString}-quays-and-props-selection-amsterdam.json`,
+        JSON.stringify(
+          // Only include results from Amsterdam
+          quaysAndPropsSelectionAmsterdam
+        ),
+        "utf8",
+        function (err) {
+          if (err) console.error(err);
+        }
+      );
+      fs.writeFile(
+        `${latestCHBExportName()}-quays-and-props-selection-amsterdam.json`,
+        JSON.stringify(
+          // Only include results from Amsterdam
+          quaysAndPropsSelectionAmsterdam
+        ),
+        "utf8",
+        function (err) {
+          if (err) console.error(err);
+        }
+      );
       console.log("Done");
     });
   });
@@ -93,8 +215,29 @@ const parseCHBXMLtoJSON = () =>
 scrape(scraperOptions)
   .then(() => copyLatest())
   .then(() =>
-    gunzip(ExportCHBLatestZip, ExportCHBLatestXML, () => {
-      console.log(`${ExportCHBLatestZip} unzipped to ${ExportCHBLatestXML}`);
+    gunzip(constantLatestStringZip, constantLatestStringXML, () => {
+      console.log(
+        `${constantLatestStringZip} unzipped to ${constantLatestStringXML}`
+      );
       parseCHBXMLtoJSON();
     })
   );
+
+function getCardinalDirectionShort(angle) {
+  const directionsShort = ["↑", "↗", "→", "↘", "↓", "↙", "←", "↖"];
+  return directionsShort[Math.round(angle / 45) % 8];
+}
+
+function getCardinalDirectionFull(angle) {
+  const directionsFull = [
+    "Noord",
+    "Noordoost",
+    "Oost",
+    "Zuidoost",
+    "Zuid",
+    "Zuidwest",
+    "West",
+    "Noordwest",
+  ];
+  return directionsFull[Math.round(angle / 45) % 8];
+}
